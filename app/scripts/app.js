f31e13ce97194b0636739f7448d0360cd707beee
bgilm <b.gil@me.com>
2016-09-07 15:46:22 -0500

    Inicial
    Proyecto Inicial

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name examenCurrencyApp
+ * @description
+ * # examenCurrencyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('examenCurrencyApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name examenCurrencyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the examenCurrencyApp
+ */
+angular.module('examenCurrencyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
