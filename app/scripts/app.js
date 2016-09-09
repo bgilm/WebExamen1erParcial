cb5804fee14838d1253f8dec710c8ee6599197db
bgilm <b.gil@me.com>
2016-09-09 17:52:32 -0500

    Pedero

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,13 +8,15 @@
  *
  * Main module of the application.
  */
-var App= angular.module('examenCurrencyApp', []);
+var App= angular.module('examenCurrencyApp', ['angularjs-dropdown-multiselect']);
 
 App.controller('listaTodoController',function($scope,$http, $interval){
-
+    
   $interval(
   $http.get('../monedas.json').then(function(res){
     $scope.monedas=res.data;
+    $scope.monedasmodel=[];
+   
   }), 5000);
 
   $scope.convertir = function(cantidad, valor){
