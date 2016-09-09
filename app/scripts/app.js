c77eb6c663c2b59edd979ef82f352a764df4d979
andresBancompara <andres@bancompara.mx>
2016-09-09 14:29:09 -0500

    script

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,11 +10,13 @@
  */
 var App= angular.module('examenCurrencyApp', []);
 
-App.controller('listaTodoController',function($scope,$http){
+App.controller('listaTodoController',function($scope,$http, $interval){
 
+  $interval(
   $http.get('../monedas.json').then(function(res){
     $scope.monedas=res.data;
-  });
+  }), 5000);
+
   $scope.convertir = function(cantidad, valor){
     var resultado = 0.0;
     resultado = cantidad*valor;
